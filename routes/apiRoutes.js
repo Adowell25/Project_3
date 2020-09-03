ecf38456b096132a05de9d44505969459ac3c844
akaCosmicRay <codie.l.mitchell@gmail.com>
2020-09-02 19:29:01 -0500

    Updated API
    Updated API to take in zipCode instead of lat and lon.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,9 +6,9 @@ var axios = require("axios");
 module.exports = function (app) {
     
     app.get("/weather", (req, res) => {
-        var lat=req.query.lat;
-        var lon=req.query.lon;
-        axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,current,minutely&appid=" + keys.openweather.id)
+
+        var zipCode = req.query.zipCode;
+        axios.get("https://api.openweathermap.org/data/2.5/onecall?zip=" + zipCode + "&exclude=hourly,current,minutely&appid=" + keys.openweather.id)
             .then(function (response) {
                 res.json(response.data);
 
