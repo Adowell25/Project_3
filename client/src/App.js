63f53e0738aa94e020b8cb7daf458c912202b714
Jordan <jordankbassett@gmail.com>
2020-09-03 18:44:49 -0500

    auth0

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import PersistentDrawerLeft from "./components/Drawer";
 import './App.css';
+import Loginbutton from "./components/loginbutton";
+import Logoutbutton from "./components/logoutbutton";
 
 function App() {
   return (
     <div className="App">
       <PersistentDrawerLeft/>
+      <Loginbutton/>
+      <Logoutbutton/>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import {Auth0Provider} from '@auth0/auth0-react';
+require("dotenv").config()
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+);
+
+ReactDOM.render( <Auth0Provider
+ 
+  domain= {process.env.REACT_APP_AUTH0_DOMAIN}
+  clientId= {process.env.REACT_APP_AUTH0_CLIENT_ID}
+  redirectUri={window.location.origin}
+>
+  <App />
+</Auth0Provider>,
+document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
