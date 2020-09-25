0ae928bbe4bf307fc087c186b1ed2677a96ef051
Adam Wolff <adamkwolff@gmail.com>
2020-09-25 04:10:17 -0500

    Turning in Project, Will Finish Later.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import PersistentDrawerLeft from "./components/Drawer";
 import './App.css';
 // import Router from "../../routes/htmlRoutes"
 // import { BrowserRouter } from "react-router-dom";
+import Loginbutton from "./components/loginbutton";
 
 function App() {
   return (
